b04fdd4cf59d6c2920511d17f8402e5ebc598a1e
irvipinyadav <irvipin7.gmail.com>
2023-10-15 21:02:52 +0530

    Initial commit from Create Next App

---
+++ src/app/test/page.tsx
@@ -0,0 +1,16 @@
+import { url } from 'inspector';
+import React from 'react'
+
+
+const page = () => {
+    console.log(url);
+    
+  return (
+    <div>
+        <button>klj;lkjkl</button>
+      
+    </div>
+  )
+}
+
+export default page
